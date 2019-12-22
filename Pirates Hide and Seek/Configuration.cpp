6499b2bdce5bfaefb093f28474a239045298af8f
Adrian Panaintescu <adrian.cert@gmail.com>
2019-12-22 12:47:32 +0200

    Write Configuration function writed Issue fix

--- Pirates Hide and Seek/Configuration.cpp
+++ Pirates Hide and Seek/Configuration.cpp
@@ -1,4 +1,3 @@
-
 #include "Configuration.h"
 
 dictionaty::dictionaty() {
@@ -39,29 +38,49 @@ bool dictionaty::ReadConfiguration(std::string filename){
 }
 
 bool dictionaty::WriteConfiguration(std::string filename){
-	return true;
+	key* tmp = this->head;
+	std::fstream file;
+	file.open(filename.c_str());
+	if (file.is_open()) {
+		bool ok = true;
+		do {
+			file << tmp->Left << " " << tmp->Right << std::endl;
+			if (tmp->Next == NULL)
+				ok = false;
+			tmp = tmp->Next;
+		} while (ok);
+		file.close();
+		return true;
+	}
+	return false;
 }
 
 bool dictionaty::GetParm(std::string s, std::string& result){
 	key* tmp = this->head;
+	bool ok = true;
 	do {
 		if (s == tmp->Left) {
 			result = tmp->Right;
 			return true;
 		}
+		if (tmp->Next == NULL)
+			ok = false;
 		tmp = tmp->Next;
-	} while (tmp->Next != NULL);
+	}while(ok);
 	return false;
 }
 
 bool dictionaty::GetParm(std::string s, int& result){
 	key* tmp = this->head;
+	bool ok = true;
 	do {
 		if (s == tmp->Left) {
 			result = std::stoi(tmp->Right);
 			return true;
 		}
+		if (tmp->Next == NULL)
+			ok = false;
 		tmp = tmp->Next;
-	} while (tmp->Next != NULL);
+	} while (ok);
 	return false;
 }
