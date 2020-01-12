51932ae38fa3153c89f3c6117794c87c8ce327fd
JogZ <stanciuvalentin7@gmail.com>
2020-01-12 03:51:00 +0200

    matriceeeeeeee

--- Pirates Hide and Seek/GameMatrix.h
+++ Pirates Hide and Seek/GameMatrix.h
@@ -1,1 +1,14 @@
-#pragma once
+#include "GameVariable.h"
+#include "Level.h"
+
+#ifndef GAMEMATRIX_H
+#define GAMEMATRIX_H
+
+void getMatriceTabla(int v[6][6]);
+void afisareMatrice();
+void GetIslaceMat(int Islace, int V[3][3]);
+void setRotation(int v[3][3], int r);
+void CompareCadran(int piesa[3][3], int cadran[3][3], int s[2]);
+void GetUncoveredItems(lvl::State* x, int* v);
+#endif // !GAMEMATRIX_H
+
