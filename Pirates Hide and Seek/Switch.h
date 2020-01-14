9a3f05eb55eddf59f820f8628f5f90d35eb55d64
Panaintescu Adrian <adrian.cert@gmail.com>
2020-01-14 18:36:01 +0200

    Switch pieces (#13)
    * deocamdata din inafara tablei pe tabla
    
    * functia GetMove merge
    
    * stable switch pieces function
    
    * functie rotatie
    
    * compare state function
    
    * sync with ceva
    
    * add bool comparestate function
    
    Co-authored-by: JoggingMist <56320456+JoggingMist@users.noreply.github.com>

--- Pirates Hide and Seek/Switch.h
+++ Pirates Hide and Seek/Switch.h
@@ -8,4 +8,5 @@ int VerPoz(int newPoz, lvl::State* state);
 bool isRelevant(int islace, lvl::State* state);
 void GetMove(int islace, int newPoz, lvl::State* state);
 void GetRotation(int islace, lvl::State* state);
+bool CompareState(lvl::State* a, lvl::State* b);
 #endif // !SWITCH_H
