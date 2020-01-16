e968e85ece9487db783ffecbf40ec316ec7eea0c
Adrian Panaintescu <adrian.cert@gmail.com>
2020-01-16 12:08:15 +0200

    Intro Improve

--- Pirates Hide and Seek/Intro.cpp
+++ Pirates Hide and Seek/Intro.cpp
@@ -2,7 +2,7 @@
 #include "menu.h"
 
 
-void resPozText(sf::Text &text)
+void resPozText(sf::Text& text)
 {
 	text.setOrigin(
 		text.getGlobalBounds().left + text.getGlobalBounds().width / 2,
@@ -12,52 +12,25 @@ void resPozText(sf::Text &text)
 bool Intro(SceneManager* sceneManager) {
 
 	sf::SoundBuffer buffer;
-	
+
 	sceneManager->RenderWindow->setMouseCursorVisible(false);
 	float angle(0.f);
-	float move(10);
+	float move(0);
 	sf::Clock time;
-	sf::Vector2f scalable = sf::Vector2f(1,1);
+	sf::Vector2f scalable = sf::Vector2f(1, 1);
 
 	sf::Font font;
-	if (!font.loadFromFile("Resource/fontTitlu.ttf")	||
+	if (!font.loadFromFile("Resource/fontTitlu.ttf") ||
 		!buffer.loadFromFile("Resource/introsound.wav"))
 		EXIT_FAILURE;
 
 	sf::Sound intro;
 	intro.setBuffer(buffer);
 	intro.play();
-	while (time.getElapsedTime() <= sf::seconds(6))
-	{
-		//Titlu Joc
-		
-		sf::Text titlu;
-		titlu.setFont(font);
-		titlu.setString("PIRATES HIDE N' SEEK");
-		titlu.setCharacterSize(100);
-		titlu.setFillColor(sf::Color::Black);
-		resOriginText(titlu);
-		titlu.setPosition(sceneManager->RenderWindow->getSize().x /2, sceneManager->RenderWindow->getSize().y / 2);
-		
-		//Patrat Incarcare
-		sf::RectangleShape rectangle;
-		rectangle.setSize(sf::Vector2f(50, 50));
-		rectangle.setFillColor(sf::Color::Color(255, 204, 102));
-		rectangle.setOrigin(25,25);
-		rectangle.setPosition(sceneManager->RenderWindow->getSize().x/2, sceneManager->RenderWindow->getSize().y/2);
-		rectangle.setRotation(angle);
-		rectangle.scale(scalable);
-		angle += 0.125f;
-		scalable.x += 0.05f;
-		scalable.y += 0.06f;
-		sceneManager->RenderWindow->clear();
-		sceneManager->RenderWindow->draw(rectangle);
-		sceneManager->RenderWindow->draw(titlu);
-		sceneManager->RenderWindow->display();
-	}
 	while (sceneManager->RenderWindow->getSize().y / 2 - move > 0)
 	{
 		//Titlu Joc
+
 		sf::Text titlu;
 		titlu.setFont(font);
 		titlu.setString("PIRATES HIDE N' SEEK");
@@ -65,7 +38,7 @@ bool Intro(SceneManager* sceneManager) {
 		titlu.setFillColor(sf::Color::Black);
 		resOriginText(titlu);
 		titlu.setPosition(sceneManager->RenderWindow->getSize().x / 2, sceneManager->RenderWindow->getSize().y / 2 - move);
-		move += 10;
+
 		//Patrat Incarcare
 		sf::RectangleShape rectangle;
 		rectangle.setSize(sf::Vector2f(50, 50));
@@ -74,15 +47,20 @@ bool Intro(SceneManager* sceneManager) {
 		rectangle.setPosition(sceneManager->RenderWindow->getSize().x / 2, sceneManager->RenderWindow->getSize().y / 2);
 		rectangle.setRotation(angle);
 		rectangle.scale(scalable);
+		angle += 0.125f;
+		scalable.x += 0.05f;
+		scalable.y += 0.06f;
+		if (scalable.y > 45) {
+			move += 10;
+		}
 		sceneManager->RenderWindow->clear();
 		sceneManager->RenderWindow->draw(rectangle);
 		sceneManager->RenderWindow->draw(titlu);
 		sceneManager->RenderWindow->display();
 	}
-	//Afiseaza cursorul inainte de a se termina intro
-	sceneManager->RenderWindow->setMouseCursorVisible(true);
 
+	sceneManager->RenderWindow->setMouseCursorVisible(true);
 	sceneManager->CurentFrame = GameEnum::GameFrame::Menu;
-	
+
 	return true;
 }
